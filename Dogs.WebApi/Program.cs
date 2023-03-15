3086f41c7d4e4b9c31dc248b80f15d5b8fbb2a82
Гафеев Глеб <gleb.gafeev@mail.ru>
2023-03-15 11:06:48 +0300

    Connect to React

--- Dogs.WebApi/Program.cs
+++ Dogs.WebApi/Program.cs
@@ -2,6 +2,8 @@ using Dogs.WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
 
 builder.Services.AddTransient<IEmailService, EmailService>();
@@ -9,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policyBuilder =>
+        {
+            policyBuilder.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -18,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
